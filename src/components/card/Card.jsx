ac7340897d6d77694e2d1c37f119a5b733c7d9c1
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-10-27 00:53:37 +0200

    style component card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ title }) => {
   return (
-    <div className='border-4 border-cyan-400 bg-red-400 h-5/6'>
+    <div className='card'>
       Card {title}
     </div>
   )
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,7 +2,7 @@ import Card from '../components/card/Card'
 
 const Layout = () => {
   return (
-    <div className='flex justify-center items-center gap-x-6 flex-wrap h-screen'>
+    <div className='flex justify-center items-center gap-4 pt-4 flex-wrap h-screen bg-slate-700'>
       <Card title='1' />
       <Card title='2' />
       <Card title='3' />
