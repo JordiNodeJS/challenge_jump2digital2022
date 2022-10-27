beda5efddeae5aac797fd2814268a474a6ec33ea
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-10-27 20:18:44 +0200

    Show svgs on the cards

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,24 @@
-const Card = ({ Svgs }) => {
+const Card = ({ Svgs, card }) => {
+  const Svg = Svgs[0]
+  // const Svg1 = (Svgs[1] ||= () => null)
+  // const Svg2 = (Svgs[2] ||= () => null)
+  if (card === 3) {
+    const Svg0 = Svgs[0]
+    const Svg1 = Svgs[1]
+    const Svg2 = Svgs[2]
+
+    return (
+      <div className='relative card flex bg-slate-400'>
+        <Svg0 className='absolute left-2' />
+        <Svg1 className='absolute left-[4.5rem]' />
+        <Svg2 className='absolute left-32' />
+      </div>
+    )
+  }
+
   return (
-    <div className='card bg-slate-400'>
-      <Svgs />
+    <div className='card flex bg-slate-400'>
+      <Svg />
     </div>
   )
 }
